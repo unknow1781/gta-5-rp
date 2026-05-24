004f2e941b99ab63bdadce1708fa69befde9060f
unknow1781 <unknow12345670@gmail.com>
2026-05-24 12:17:23 +0400

    gta 5 rp

---
+++ app.js
@@ -0,0 +1,32 @@
+function enterSite() {
+  document.getElementById("popup").style.display = "none";
+}
+
+function openTutorial() {
+  window.location.href = "https://youtu.be/RrwzhoWChwE?si=76nHeQK9HX_MvLUa";
+}
+menuToggle.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
+function enterSite(){
+
+  document.getElementById("popup").style.display = "none";
+
+}
+
+function openTutorial(){
+
+  window.location.href = "https://youtu.be/RrwzhoWChwE?si=76nHeQK9HX_MvLUa";
+
+}
+
+
+
+
+
+
+
+
+
+
+
